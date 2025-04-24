da56990dd2f0d9cfba1c9ca15f8530e7b013406b
IsabelaYabe <103371464+IsabelaYabe@users.noreply.github.com>
2025-04-24 18:47:59 -0300

    up

--- src/dataset_prepare.py
+++ src/dataset_prepare.py
@@ -1,4 +1,8 @@
-from dataclasses import dataclass, field   
+# vou produzir com ia generativa um dataset de cenários de usos
+# em seguida vou produzir um modelo de ia generativa para gerar requisitos, aqui devemos retornar um modelo de clusterização para separar os requisitos
+# e depois um modelo de ia generativa para gerar requisitos
+
+from dataclasses import dataclass   
 import pandas as pd
 import spacy
 import re
@@ -9,105 +13,96 @@ from logger import logger
 
 @dataclass(frozen=True, slots=True)
 class UserStory:
-        id: str
-        text: str
-        type_of_user: str 
-        goal: str 
-        reason: str 
-        requirement: str
-        doc: str
-        metadata: str
-        verbs: list[str] = field(default_factory=list)
-        auxs: list[str] = field(default_factory=list)
-        nouns: list[str] = field(default_factory=list) 
-        propns: list[str] = field(default_factory=list)
-        prons: list[str] = field(default_factory=list) 
-        adjs: list[str] = field(default_factory=list) 
-        advs: list[str] = field(default_factory=list)
-        sconjs: list[str] = field(default_factory=list) 
-        parts: list[str] = field(default_factory=list) 
-        orgs: list[str] = field(default_factory=list)
+    id: str
+    text: str
+    type_of_user: str
+    goal: str
+    reason: str
+    requirement_id: str
+    req_doc_id: str
+    metadata_id: str
+
+@dataclass
+class Metadata:
+    id: str
+    doc_name: str
+    text: str
+    req_doc_id: str
 
 @dataclass(frozen=True, slots=True)
 class UsageScenario:
-        id: str
-        text: str
-        requirements: str
-        verbs: list[str] = field(default_factory=list)
-        auxs: list[str] = field(default_factory=list)
-        nouns: list[str] = field(default_factory=list) 
-        propns: list[str] = field(default_factory=list)
-        prons: list[str] = field(default_factory=list) 
-        adjs: list[str] = field(default_factory=list) 
-        advs: list[str] = field(default_factory=list)
-        sconjs: list[str] = field(default_factory=list) 
-        parts: list[str] = field(default_factory=list) 
-        orgs: list[str] = field(default_factory=list)
-
+    id: str
+    text: str
+    req_doc_id: str
+        
 @dataclass(frozen=True, slots=True)
 class Requirement:
-        id: str 
-        key: str
-        text: str
-        metadata: str
-        user_stories: list[str] = field(default_factory=list)
-        usage_scenarios: list[str] = field(default_factory=list)
-        verbs: list[str] = field(default_factory=list)
-        auxs: list[str] = field(default_factory=list)
-        nouns: list[str] = field(default_factory=list) 
-        propns: list[str] = field(default_factory=list)
-        prons: list[str] = field(default_factory=list) 
-        adjs: list[str] = field(default_factory=list) 
-        advs: list[str] = field(default_factory=list)
-        sconjs: list[str] = field(default_factory=list) 
-        parts: list[str] = field(default_factory=list) 
-        orgs: list[str] = field(default_factory=list)
+    id: str
+    key: str
+    text: str
+    metadata_id: str
+    req_doc_id: str
 
 @dataclass(frozen=True, slots=True)
 class RequirementDocumentation:
-        id: str
-        title: str
-        text: str
-        documentation_path: str
-        metadata: str
-        requirements: list[Requirement] = field(default_factory=list)
-        user_stories: list[UserStory] = field(default_factory=list)
-        usage_scenarios: list[UsageScenario] = field(default_factory=list)
+    id: str
+    title: str
+    text: str
+    file_path: str
+    metadata_id: str
 
+@dataclass(frozen=True, slots=True)
+class ReqUsageScenarioMap:
+    requirement_id: str
+    usage_scenario_id: str
+
+@dataclass(frozen=True, slots=True)
+class UserStoriesUsageScenariosMAp:
+    user_storie_id: str
+    usage_scenario_id: str
+
+@dataclass(frozen=True, slots=True)
+class MetadataUsageScenarioMap:
+    metada_id: str
+    usage_scenario_id: str
+        
 # pure_req_user_stories_path = os.path.join("data", "pure_req_user_stories.csv")
 # pure_req_us_df = pd.read_csv(pure_req_user_stories_path)
+# cols df: filename,keys,requirement,user story llama-4-maverick,user story llama-3-3-70b,user story llama-3-1-405b,user story dbrx,user story mixtral-8x7b
 class DatasetPrepare:
-    def __init__(self, csv_file, req_doc_path, raw_text_path, doc_struct_path, pattern=r"As (.*?), I want (.*?) so that (.*?)(?:\.|$)", nlp=spacy.load("en_core_web_sm")):
-        self._csv_file = csv_file
-        self._req_doc_path = req_doc_path
-        self._raw_text_path = raw_text_path
+    def __init__(self, req_user_stories_dataset, raw_text_doc_path, doc_struct_path, doc_path, pattern=r"As (.*?), I want (.*?) so that (.*?)(?:\.|$)", nlp=spacy.load("en_core_web_sm")):
+        self._req_user_stories_dataset = req_user_stories_dataset
+        self._raw_text_doc_path = raw_text_doc_path
         self._doc_struct_path = doc_struct_path       
+        self._doc_path = doc_path
         self._pattern = pattern
         self._nlp = nlp
-        self._requeriments_extracted,self._user_stories_extracted, self._usage_scenarios_extracted, self._req_docs_extracted, self._docs_metadatas = self.__get_all_data()
-        self._dataframe = self._get_dataframe()
+        #self._requeriments_extracted,self._user_stories_extracted, self._usage_scenarios_extracted, self._req_docs_extracted, self._docs_metadatas = self.__get_all_data()
 
-    def _extract_user_story_parts(self, text):
-        user_story = []
-        user_story_parts = []
+    def _extract_user_story_parts(self, text, requirement_id, req_doc_id, metadata_id):
+        user_stories = []
+
         logger.debug(f"Extracting user story parts from text: {text}")
-        pattern = r"As (.*?), I want (.*?) so that (.*?)(?:\.|$)"
         try:
-            matches = list(re.findall(pattern, text, re.IGNORECASE))
+            matches = list(re.findall(self._pattern, text, re.IGNORECASE))
             num_matches = len(matches)
 
-            for i in range(num_matches):            
-                user_story.append(f"As {matches[i][0]}, I want {matches[i][1]} so that {matches[i][2]}.")
-                user_story_parts.append({
-                    "type of user": "".join(matches[i][0].strip(" ")[1:]),
+            for i in range(num_matches):       
+                user_story_datas = {
+                    "id": requirement_id + f"us.{num_matches}",
+                    "text": f"As {matches[i][0]}, I want {matches[i][1]} so that {matches[i][2]}.",             
+                    "type of user": matches[i][0],
                     "goal": matches[i][1],
-                    "reason": matches[i][2]
-                })
+                    "reason": matches[i][2],
+                    "requirement_id": requirement_id,
+                    "req_doc_id": req_doc_id,
+                    "metadata_id": metadata_id
+                }
+                user_stories.append(UserStory(**user_story_datas))
                 
-            return user_story, user_story_parts
         except IndexError:
             logger.error(f"Error extracting user story parts from text (r'As a (.*?), I want (.*?) so that (.*?)(?:\.|$)'): {text}")
-            return [], []
+        return user_stories
 
     def _get_dataframe(self):
         logger.debug("Starting the dataset preparation process")
@@ -130,65 +125,28 @@ class DatasetPrepare:
 
         return pure_req_us_df
 
-    def _extract_metadatas(self, metadata_doc):
+    def _extract_metadatas(self,  dir, file, metadata_doc, req_doc_id):
         result = {}
-        pattern = re.compile(r"^([A-Z]|\d+(\.\d+)*)(\s+)(.+)")
+        lines = metadata_doc.split("\n")
+        doc_name = " ".join(lines[0].split()[1:])
+        lines = lines[1:]
 
-        for line in metadata_doc:
-            line = line.strip()
+        for line in lines:
             if not line:
                 continue
 
-            match = pattern.match(line)
-            if match:
-                key = match.group(1)
-                text = match.group(4)
-                result[key] = text
-
+            line = line.split()    
+            key = line[0]
+            text = " ".join(line[1:])
+            
+            metadata_datas = {
+                "id": file + f".{key}",
+                "doc_name": doc_name,
+                "text": text,
+                "req_doc_id": req_doc_id
+            }
         return result
 
-    def _extract_token_pos(self, text):
-        doc = self._nlp(text)
-        logger.debug("Starting the tokenization and POS tagging process")   
-
-        verb = [] # Verbo (should provide, enable, etc.)
-        aux = [] # Auxiliar (should, must, can, is, are, etc.)
-        noun = [] # Substantivo (system, user, report, etc.)
-        propn = [] # Substantivo próprio (Nome da empresa, nome de funcionário, etc.)
-        pron = [] # Pronome (user, it, they, he, she, etc.)
-        adj = [] # Adjetivo (detailed, new, old, etc.)
-        adv = [] # Advérbio (automatically, only, quickly, slowly, etc.)
-        sconj = [] # Conjunção subordinada (so, that, when, if, because, etc.)
-        part = [] # Particípio (to provide, to allow, should be, should have, etc.)
-        org = [] # Organização (Nome da empresa, nome de funcionário, etc.)
-        
-        for token in doc:
-            token_text = token.text
-            pos = token.pos_
-
-            if pos == "VERB":
-                verb.append(token_text)
-            elif pos == "AUX":
-                aux.append(token_text)
-            elif pos == "NOUN":
-                noun.append(token_text)
-            elif pos == "PROPN":
-                propn.append(token_text)
-            elif pos == "PRON":
-                pron.append(token_text)
-            elif pos == "ADJ":
-                adj.append(token_text)
-            elif pos == "ADV":
-                adv.append(token_text)
-            elif pos == "SCONJ":
-                sconj.append(token_text)
-            elif pos == "PART":
-                part.append(token_text)
-            elif pos == "ORG":
-                org.append(token_text)
-        logger.debug("Token classification completed")
-        return verb, aux, noun, propn, pron, adj, adv, sconj, part, org
-    
     def _extract_text_from_file(self, dir, file):
         text = ""
         if file.endswith(".txt"):
